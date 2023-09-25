e2b655655050d2846fc80c0fa4cd5f94b4a0d38f
David Granado <davidjgranado@gmail.com>
2023-09-24 21:23:00 -0500

    Add landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ async function HomePage() {
 	const session = await getServerSession();
 	return (
 		<div className="min-h-screen items-center justify-between p-24">
+			<p>
+				Landing
+			</p>
 			{session && (
 				<>
 					Logged in as {session.user.username}
